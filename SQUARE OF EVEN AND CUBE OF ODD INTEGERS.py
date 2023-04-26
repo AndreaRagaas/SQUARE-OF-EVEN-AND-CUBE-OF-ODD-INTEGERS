404737695a10e25871945943a47f1111496fd63d
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-04-26 12:15:08 +0800

    creating two files separating the square of even and cube of odd integers

--- SQUARE OF EVEN AND CUBE OF ODD INTEGERS.py
+++ SQUARE OF EVEN AND CUBE OF ODD INTEGERS.py
@@ -1,2 +1,5 @@
 #opening the integers file
 my_file = open("integers.txt","r")
+
+#creating two files separating the square of even and cube of odd integers
+numbers = [int(x) for x in my_file.read().split()]
