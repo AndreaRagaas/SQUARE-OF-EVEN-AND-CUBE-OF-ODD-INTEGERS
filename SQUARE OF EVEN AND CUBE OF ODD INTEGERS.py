f34cfaab40a4ab03d4fb5e7e2bb912da352996c0
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-04-26 12:16:19 +0800

    write square numbers to double.txt

--- SQUARE OF EVEN AND CUBE OF ODD INTEGERS.py
+++ SQUARE OF EVEN AND CUBE OF ODD INTEGERS.py
@@ -3,3 +3,9 @@ my_file = open("integers.txt","r")
 
 #creating two files separating the square of even and cube of odd integers
 numbers = [int(x) for x in my_file.read().split()]
+
+#write square numbers to double.txt
+with open("double.txt", "w") as square_file:
+    for num in numbers:
+        if num % 2 == 0:
+            square_file.write(str(num**2) + '\n')
