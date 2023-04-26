5bb6072b89cc878843590e732ccfabe2a7327743
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-04-26 12:16:48 +0800

    write cube numbers to triple.txt

--- SQUARE OF EVEN AND CUBE OF ODD INTEGERS.py
+++ SQUARE OF EVEN AND CUBE OF ODD INTEGERS.py
@@ -9,3 +9,9 @@ with open("double.txt", "w") as square_file:
     for num in numbers:
         if num % 2 == 0:
             square_file.write(str(num**2) + '\n')
+
+#write cube numbers to triple.txt
+with open("triple.txt", "w") as cube_file:
+    for num in numbers:
+        if num % 2 != 0:
+            cube_file.write(str(num**3) + '\n')
